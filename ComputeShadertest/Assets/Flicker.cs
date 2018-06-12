2cb142badcdcda56d0a4bb32796f96b8294ee37b
jetsontwo <jetsontwo@gmail.com>
2018-06-12 12:40:59 -0700

    I HAVE MADE FIRE!

---
+++ ComputeShadertest/Assets/Flicker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour {
+
+
+    Light l;
+
+	// Use this for initialization
+	void Start () {
+        l = GetComponent<Light>();
+        StartCoroutine(flick());
+	}
+	
+	IEnumerator flick()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(0.03f, 0.1f));
+            l.intensity = Random.Range(0.2f, 1.2f);
+        }
+    }
+}
